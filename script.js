819990382794a6fd8e87320cb310f5de77f081fb
Yoann9808 <yoann.pouliquen92@gmail.com>
2025-09-19 14:11:52 +0200

    update

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ fetch("https://whatyearisit-backend-sooty-psi.vercel.app/year")
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
-    document.querySelector("#year").textContent += data.year.stringify();
+    document.querySelector("#year").textContent += data.year;
   });
